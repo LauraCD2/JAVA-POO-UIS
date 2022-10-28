014f07b8b8d32cd6652d24b142a83a0435db3dbc
Lali <lauracamiladiazdelgado@gmail.com>
2022-10-27 20:30:29 -0500

    Add files via upload

---
+++ LAB2CASA/app.java
@@ -0,0 +1,10 @@
+//permita establecer e imprimir el nombre del propietario y la superficie de la casa.
+public class app {
+    public static void main(String[] args) {
+        casa casa1 = new casa();
+        casa1.setPropietario("Juan");
+        casa1.setSuperficie(100);
+        casa1.visualizar();
+        
+    }
+}
---
+++ LAB2CASA/casa.java
@@ -0,0 +1,27 @@
+//Crear una clase casa que permita guardar el nombre del propietario y la superficie de la casa. 
+//Permitir a la clase casa establecer, devolver y visualizar el propietario y la superficie de la casa.
+public class casa {
+    private String propietario;
+    private double superficie;
+    
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+    
+    public String getPropietario() {
+        return propietario;
+    }
+    
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+    
+    public double getSuperficie() {
+        return superficie;
+    }
+    
+    public void visualizar() {
+        System.out.println("Propietario: " + propietario);
+        System.out.println("Superficie: " + superficie);
+    }
+}
